a566048703f2b0b225d26549969dfd6f33e288b2
Ulum Sai <saiful.ulum@aro.co.id>
2024-09-04 13:53:07 +0700

    setup docker

--- restart-deploy.sh
+++ restart-deploy.sh
@@ -3,7 +3,6 @@ docker rm genai-app
 docker rmi genai-image
 docker-compose up build
 docker-compose up -d
-
-docker-compose exec -T app mkdir logs
-docker-compose exec -T app chmod -R 777 logs
-docker-compose exec -T app chmod -R 777 core/docs
+# docker-compose exec -T app mkdir logs
+# docker-compose exec -T app chmod -R 777 logs
+# docker-compose exec -T app chmod -R 777 core/docs
