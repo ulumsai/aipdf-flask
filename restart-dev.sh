63cbc91118c572949f48e0d5737921b5ca7b194b
Ulum Sai <saiful.ulum@aro.co.id>
2024-09-05 06:53:02 +0700

    sh

--- restart-dev.sh
+++ restart-dev.sh
@@ -1,2 +1,2 @@
-docker-compose -f docker-compose-local.yml down
-docker-compose -f docker-compose-local.yml up -d
+docker-compose -f docker-compose-dev.yml down
+docker-compose -f docker-compose-dev.yml up -d
